66e3995fe26a0a7c407e680d721aaf405918439d
Nicklas Utgaard <nutgaard@gmail.com>
2016-05-16 14:15:12 +0200

    Consolidated configuration into one file

--- src/main/java/no/utgdev/ssrr/config/AppConfig.java
+++ src/main/java/no/utgdev/ssrr/config/AppConfig.java
@@ -1,10 +1,15 @@
 package no.utgdev.ssrr.config;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class AppConfig extends ResourceConfig {
+
+    public AppConfig() {
+        packages("no.utgdev.ssrr.api");
+    }
 
     @Bean(name = "testbean")
     public String test() {
